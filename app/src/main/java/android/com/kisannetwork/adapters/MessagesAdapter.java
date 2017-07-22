80d772b745b38b3038ee6a97afe0ecdaed6cf273
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-07-22 13:09:20 +0530

    Adds message adapter class

---
+++ app/src/main/java/android/com/kisannetwork/adapters/MessagesAdapter.java
@@ -0,0 +1,94 @@
+package android.com.kisannetwork.adapters;
+
+import android.com.kisannetwork.R;
+import android.com.kisannetwork.listeners.ClickListener;
+import android.com.kisannetwork.model.Contact;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by shobhit on 21/7/17.
+ */
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+public class MessagesAdapter extends
+        RecyclerView.Adapter<MessagesAdapter.ViewHolder> {
+
+
+    private final ClickListener listener;
+    private List<Contact> contacts;
+    private Context context;
+
+    // Pass in the contact array into the constructor
+    public MessagesAdapter(Context context, List<Contact> contacts, ClickListener listener) {
+        this.contacts = contacts;
+        this.context = context;
+        this.listener = listener;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return context;
+    }
+
+    @Override
+    public MessagesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(contactView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(MessagesAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Contact contact = contacts.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        TextView nameTextView;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/android/com/kisannetwork/fragments/SentSMSFragment.java
+++ app/src/main/java/android/com/kisannetwork/fragments/SentSMSFragment.java
@@ -1,18 +1,34 @@
 package android.com.kisannetwork.fragments;
 
 import android.com.kisannetwork.R;
+import android.com.kisannetwork.activities.ContactDetailsActivity;
+import android.com.kisannetwork.adapters.MessagesAdapter;
+import android.com.kisannetwork.listeners.ClickListener;
+import android.com.kisannetwork.model.Contact;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by shobhit on 21/7/17.
  */
 
-public class SentSMSFragment extends Fragment {
+public class SentSMSFragment extends Fragment implements ClickListener {
+
+    ArrayList<Contact> contacts;
 
     public SentSMSFragment() {
 
@@ -27,6 +43,63 @@ public class SentSMSFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+        loadData();
+
+        MessagesAdapter adapter = new MessagesAdapter(getContext(), contacts, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return rootView;
+    }
+
+    private void loadData() {
+        try {
+            contacts = new ArrayList<>();
+            JSONArray jsonArray = new JSONArray(loadJSONFromAsset());
+
+            Contact contact;
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+
+                contact = new Contact();
+                contact.setAddress(object.getString("address"));
+                contact.setPhoneNumber(object.getString("phone"));
+                contact.setEmail(object.getString("email"));
+                contact.setCompany(object.getString("company"));
+                contact.setFirstName(object.getJSONObject("name").getString("first"));
+                contact.setLastName(object.getJSONObject("name").getString("last"));
+                contact.setAge(object.getInt("age"));
+                contact.setImage(object.getString("picture"));
+                contact.setIndex(object.getInt("index"));
+
+                contacts.add(contact);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getActivity().getAssets().open("contacts.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    @Override
+    public void onClick(int position) {
+        Contact contact = contacts.get(position);
+        Intent intent = new Intent(getActivity(), ContactDetailsActivity.class);
+        intent.putExtra("contact", contact);
+        getActivity().startActivity(intent);
     }
 }
