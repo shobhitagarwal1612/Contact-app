caf6021a069268b2c351c59ceb00139847d97c50
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-07-22 22:58:19 +0530

    Adds interface dataUpdated Layout xml for the message history item

---
+++ app/src/main/java/android/com/kisannetwork/database/DbUtils.java
@@ -0,0 +1,43 @@
+package android.com.kisannetwork.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by shobhit on 22/7/17.
+ */
+
+public class DbUtils {
+
+    public static Cursor getMessagesHistoryCursor(Context context) {
+
+        MessagesHistory.MessagesDbHelper dbHelper = new MessagesHistory.MessagesDbHelper(context);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                MessagesHistory.MessageEntry._ID,
+                MessagesHistory.MessageEntry.COLUMN_NAME_CONTACT_NAME,
+                MessagesHistory.MessageEntry.COLUMN_NAME_OTP,
+                MessagesHistory.MessageEntry.COLUMN_NAME_TIME
+        };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                MessagesHistory.MessageEntry.COLUMN_NAME_TIME + " DESC";
+
+        Cursor cursor = db.query(
+                MessagesHistory.MessageEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                null,                                     // The columns for the WHERE clause
+                null,                                     // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        return cursor;
+    }
+}
---
+++ app/src/main/java/android/com/kisannetwork/listeners/DataUpdated.java
@@ -0,0 +1,10 @@
+package android.com.kisannetwork.listeners;
+
+/**
+ * Created by shobhit on 22/7/17.
+ */
+
+public interface DataUpdated {
+
+    void update();
+}
