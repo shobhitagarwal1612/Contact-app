16eca83b3faa7895b3f518d1539909e389ec7baa
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-07-23 11:40:39 +0530

    Code refactoring

---
+++ app/src/main/java/android/com/kisannetwork/Constants.java
@@ -0,0 +1,10 @@
+package android.com.kisannetwork;
+
+/**
+ * Created by shobhit on 23/7/17.
+ */
+
+public class Constants {
+    public static final String ACCOUNT_SID = "ACe0b238b269adc92973d35be7397373c3";
+    public static final String AUTH_TOKEN = "5bfefc3693e495edde3019892cc00fc2";
+}
